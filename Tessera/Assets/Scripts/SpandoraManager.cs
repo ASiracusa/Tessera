b9be3c179aa2979cf2110eeda3bda807cceacfa0
ASiracusa <31045746+ASiracusa@users.noreply.github.com>
2025-05-28 23:43:52 -0400

    Add Letter Values

--- Tessera/Assets/Scripts/SpandoraManager.cs
+++ Tessera/Assets/Scripts/SpandoraManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using TMPro;
+using static Constants;
 
 public class SpandoraManager : MonoBehaviour
 {
@@ -11,7 +13,13 @@ public class SpandoraManager : MonoBehaviour
         {
             for (int c = -2; c <= 2; c++)
             {
-                Instantiate(diePrefab, new Vector3(20 * c, 20 * r, 0), Quaternion.identity);
+                int letterInd = Random.Range(0,26);
+                char letter = (char) (65 + letterInd);
+                GameObject letterDie = Instantiate(diePrefab, new Vector3(20 * c, 20 * r, 0), Quaternion.identity);
+                GameObject textCenter = letterDie.transform.Find("DieCanvas/TextCenter").gameObject;
+                textCenter.GetComponent<TMP_Text>().text = letter.ToString();
+                GameObject textBottom = letterDie.transform.Find("DieCanvas/TextBottom").gameObject;
+                textBottom.GetComponent<TMP_Text>().text = Constants.TILE_VALUES[letter].ToString();
             }
         }
     }
